073c1ef2a739387b60a8cc016055d4446cb65fbc
devopspk93 <devopspk93@gmail.com>
2024-08-13 00:52:42 +0530

    final-test

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
-echo "Hello, World from Docker! <br>";
-echo "Welcome to website ! <br>";
-echo "Hi This side company HR";
+echo "Hello, GitHub-Docker-jenkins-minikube! <br>";
+echo "Welcome to this website ! <br>";
+echo "we are comming soon";
 
 ?>
