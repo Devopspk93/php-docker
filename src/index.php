8a55ab5d8f64b6f8d8efc3b283ff03cf23423d18
Devopspk93 <devopspk93@gmail.com>
2024-12-12 09:33:14 +0530

    Update index.php
    Dec

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Hello, GitHub-Docker-jenkins-minikube pod-:<br>";
-echo "Welcome to this website WWWEEE$<br>";
+echo "Welcome to this website WWWESP$<br>";
 echo "we are comming soon in your country";
 
 ?>
