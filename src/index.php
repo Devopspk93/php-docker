09791cbd01a1f543ff7e22021709c2bd36ba397f
Devopspk93 <devopspk93@gmail.com>
2024-12-22 21:18:53 +0530

    Update index.php
    k

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Hello, GitHub-Docker-jenkins-minikube pod-:<br>";
-echo "Welcome to this website Login!11!--<br>";
+echo "Welcome to this website Login!22!--<br>";
 echo "we are comming soon in your country";
 
 ?>
