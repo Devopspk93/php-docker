8385fac6d154c4c76c3f1230f62878f6c4da0004
Devopspk93 <devopspk93@gmail.com>
2025-02-07 23:32:58 +0530

    Update index.php
    nn

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 
 echo "Hello, GitHub-Docker-jenkins-minikube pod-:<br>";
-echo "Welcome to this website Login!67llkk--<br>";
+echo "Welcome to this website we are comming soon with another-site--<br>";
 echo "we are comming soon in your country";
 
 ?>
