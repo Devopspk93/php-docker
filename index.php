a0b9333c178d9dc346598044e11a315b14f183c8
Tirupati <tirupati_gollapelli@persistent.com>
2025-07-28 14:53:53 +0530

    Revert "Tiru commit"
    This reverts commit a3d5f5d35ddab63dce9be24264689ee59ac1b854.

--- index.php
+++ index.php
@@ -4,6 +4,4 @@ echo "Hello, GitHub-Docker-jenkins-minikube #!test#<br>";
 echo "Welcome to this website ! <br>";
 echo "we are comming soon in couple of week";
 
-echo "Adding test message : Tiru"
-
 ?>
